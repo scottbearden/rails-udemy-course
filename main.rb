f348c18ba4246b445e959f3fb1965b3d191b6d33
Scott Bearden <scott.bearden@gmail.com>
2020-06-20 15:34:01 -0400

    working with IRB

--- main.rb
+++ main.rb
@@ -2,6 +2,6 @@ puts "What is your first name?"
 first_name = gets.chomp
 puts "Thank you, #{first_name}"
 
-puts "Eneter a number to multiply by 2"
+puts "Enter a number to multiply by 2"
 input = gets.chomp
 puts input.to_i * 2
