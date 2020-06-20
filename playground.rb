ae5b97846108359e2fed93ec3eead43105d82ce1
Scott Bearden <scott.bearden@gmail.com>
2020-06-20 11:30:29 -0400

    testing put strings, variables and methods

---
+++ playground.rb
@@ -0,0 +1,16 @@
+# say hello world in multiple ways
+# simple put string
+# Using a variable
+# Using a method
+
+# puts "Hello World"
+
+#greeting = "Hello World Variable"
+#puts greeting
+
+def say_hello(thing_to_say)
+    #puts "Hello World Method"
+    puts thing_to_say
+end
+
+say_hello "Hello Scott"
