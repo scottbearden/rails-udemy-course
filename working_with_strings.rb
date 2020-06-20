1370b92bab4ba8394c96e92bb7b6e5b7b86f8905
Scott Bearden <scott.bearden@gmail.com>
2020-06-20 15:33:47 -0400

    Working with strings, IRB

---
+++ working_with_strings.rb
@@ -0,0 +1,15 @@
+# String concatenation
+first_name = "Scott"
+last_name = "Bearden"
+puts first_name + " " + last_name
+
+# String interpolation
+puts "My first name is #{first_name} and my last name is #{last_name}"
+# Methods, how to find them
+
+# Common methods
+
+# Variable assignment
+
+# Escaping
+
